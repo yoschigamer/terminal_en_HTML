06ad6e978f479b7c16208848d1a2542d5cd10774
Killian Schneider <80042061+yoschigamer@users.noreply.github.com>
2022-03-20 21:50:34 +0100

    Add files via upload

--- MON_CV.js
+++ MON_CV.js
@@ -1,13 +1,3 @@
-const progressBar = document.querySelector('.draggable')
-
-let totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-window?.addEventListener('scroll', () => {
-    let progress = (document.documentElement.scrollTop / totalHeight) * 100;
-    progressBar.style.top = `${progress}%`
-    console.log(progress)
-})
-
 const close = document.getElementById('close')
 const terminal = document.getElementById('all')
 const app = document.querySelector('#app')
@@ -23,4 +13,13 @@ close?.addEventListener('click', () => {
     app.style.display = 'block'
 });
 
+let scroll = document.querySelector("#scroll");
+var console_height = document.getElementById('console_log');
 
+scroll?.addEventListener('scroll', (event) => {
+    var console_height = parseInt(document.getElementById('console_log').clientHeight)
+    let scroll = document.querySelector("#scroll").scrollTop;
+    
+    var drag = document.querySelector("#drag").style.top = scroll / console_height  * 100 + '%';
+    console.log(`${scroll} / ${console_height} * 100 = ${drag}`)
+});
